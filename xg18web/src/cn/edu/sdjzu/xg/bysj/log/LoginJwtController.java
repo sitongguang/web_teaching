50889917870c33901d92cea08ee151a411ad4842
stone <stg@sdjzu.edu.cn>
2019-12-01 22:03:05 +0800

    + jackson jjwt 0.10

---
+++ xg18web/src/cn/edu/sdjzu/xg/bysj/log/LoginJwtController.java
@@ -0,0 +1,102 @@
+package cn.edu.sdjzu.xg.bysj.log;
+
+import cn.edu.sdjzu.xg.bysj.domain.User;
+import cn.edu.sdjzu.xg.bysj.service.UserService;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import util.JSONUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/jwt_login")
+public class LoginJwtController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User userToLogin = new User();
+        userToLogin.setUsername(username);
+        userToLogin.setPassword(password);
+
+        //设置响应字符编码为UTF-8
+        response.setContentType("text/html;charset=UTF-8");
+        //创建JSON对象message，以便往前端响应信息
+        JSONObject message = new JSONObject();
+        try {
+            User loggedUser = UserService.getInstance().login(userToLogin);
+            if(loggedUser!=null){
+                // 登录成功：
+                // 有效时间：一天
+                long ttlMillis = 24 * 60 * 60 * 1000;
+                // 昵称
+                String nickname = "star";
+                // 生成jws
+                /**
+                 *
+                 */
+                String jws = JwtUtil.createJWT(username, nickname, ttlMillis);
+                System.out.println(jws);
+                // jws可以存到数据库
+                // 返回给前端
+//                message.put("message", "登录成功");
+//                HttpSession session = request.getSession();
+//                session.setAttribute("user" , loggedUser);
+                response.getWriter().println(JSON.toJSONString(jws));
+                return;
+            }else{
+                message.put("message", "用户名或密码错误");
+            }
+        } catch (SQLException e) {
+            message.put("message", "数据库操作异常");
+            e.printStackTrace();
+        } catch (Exception e) {
+            message.put("message", "网络异常");
+            e.printStackTrace();
+        }
+        //响应message到前端
+        response.getWriter().println(message);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Request encoding="+request.getCharacterEncoding());
+        System.out.println("Response encoding="+response.getCharacterEncoding());
+
+        String user_json = JSONUtil.getJSON(request);
+        //将JSON字串解析为Degree对象
+        User userToLogin = JSON.parseObject(user_json, User.class);
+
+        //设置响应字符编码为UTF-8
+        response.setContentType("text/html;charset=UTF-8");
+        //创建JSON对象message，以便往前端响应信息
+        JSONObject message = new JSONObject();
+        try {
+            User loggedUser = UserService.getInstance().login(userToLogin);
+            if(loggedUser!=null){
+//                message.put("message", "登录成功");
+                HttpSession session = request.getSession();
+                session.setAttribute("user" , loggedUser);
+                response.getWriter().println(JSON.toJSONString(loggedUser));
+                return;
+            }else{
+                message.put("message", "用户名或密码错误");
+            }
+        } catch (SQLException e) {
+            message.put("message", "数据库操作异常");
+            e.printStackTrace();
+        } catch (Exception e) {
+            message.put("message", "网络异常");
+            e.printStackTrace();
+        }
+        //响应message到前端
+        response.getWriter().println(message);
+    }
+}
