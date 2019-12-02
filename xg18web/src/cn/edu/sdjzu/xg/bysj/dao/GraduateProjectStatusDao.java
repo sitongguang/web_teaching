86c77446ca51e0bd6bc6b04aff4889ac24d1ed67
stone <stg@sdjzu.edu.cn>
2019-12-02 10:23:08 +0800

    + throws SQLException

--- xg18web/src/cn/edu/sdjzu/xg/bysj/dao/GraduateProjectStatusDao.java
+++ xg18web/src/cn/edu/sdjzu/xg/bysj/dao/GraduateProjectStatusDao.java
@@ -3,6 +3,7 @@ package cn.edu.sdjzu.xg.bysj.dao;
 
 import cn.edu.sdjzu.xg.bysj.domain.GraduateProjectStatus;
 
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.TreeSet;
 
@@ -25,11 +26,11 @@ public final class GraduateProjectStatusDao {
         return graduateProjectStatusDao;
     }
 
-    public Collection<GraduateProjectStatus> findAll() {
+    public Collection<GraduateProjectStatus> findAll()throws SQLException {
         return GraduateProjectStatusDao.graduateProjectStatuses;
     }
 
-    public GraduateProjectStatus find(Integer id) {
+    public GraduateProjectStatus find(Integer id) throws SQLException{
         GraduateProjectStatus desiredGraduateProjectStatus = null;
         for (GraduateProjectStatus graduateProjectStatus : graduateProjectStatuses) {
             if (id.equals(graduateProjectStatus.getId())) {
@@ -39,21 +40,21 @@ public final class GraduateProjectStatusDao {
         return desiredGraduateProjectStatus;
     }
 
-    public boolean update(GraduateProjectStatus graduateProjectStatus) {
+    public boolean update(GraduateProjectStatus graduateProjectStatus) throws SQLException{
         graduateProjectStatuses.remove(graduateProjectStatus);
         return graduateProjectStatuses.add(graduateProjectStatus);
     }
 
-    public boolean add(GraduateProjectStatus graduateProjectStatus) {
+    public boolean add(GraduateProjectStatus graduateProjectStatus) throws SQLException{
         return graduateProjectStatuses.add(graduateProjectStatus);
     }
 
-    public boolean delete(Integer id) {
+    public boolean delete(Integer id) throws SQLException{
         GraduateProjectStatus graduateProjectStatus = this.find(id);
         return this.delete(graduateProjectStatus);
     }
 
-    public boolean delete(GraduateProjectStatus graduateProjectStatus) {
+    public boolean delete(GraduateProjectStatus graduateProjectStatus) throws SQLException {
         return GraduateProjectStatusDao.graduateProjectStatuses.remove(graduateProjectStatus);
     }
 }
